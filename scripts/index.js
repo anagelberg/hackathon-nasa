6a92186f7e198f50c531f69ff163d56d249da648
anagelberg <amy.nagelberg@gmail.com>
2023-07-21 12:17:46 -0700

    image display, added background

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,43 @@
-const display = (imageUrl, container) => {
-    container.style.backgroundImage = imageUrl;
+api_key = "NfrtX26rhNzjyIfRcalbFEaBQta2hTLMnSQAHrJS";
+
+
+const display = (urlArray, parentContainer) => {
+    urlArray.forEach((url) => {
+        const imageContainer = document.createElement('div');
+        imageContainer.classList.add('hero__images');
+        imageContainer.style.backgroundImage = `url('${url}')`;
+        parentContainer.appendChild(imageContainer);
+    })
 }
 
 
+const formatArray = (apiRes) => {
+    let images_array = [];
+    for (imageIndex = 0; imageIndex < 15; imageIndex++) {
+        let image = apiRes.data.collection.items[imageIndex].links[0].href;
+        images_array.push(image);
+    }
+    return images_array;
+}
+
+const form = document.querySelector('.planet-form');
+const heroEl = document.querySelector('.hero');
+console.log()
+
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log(event.target.name.value);
+    const search = 'planet ' + event.target.name.value;
+    console.log(search)
+
+    /* pipe search into axios */
+    axios.get(`https://images-api.nasa.gov/search?q=${search}&media_type=image`)
+        .then((response) => {
+            images = formatArray(response)
+            heroEl.innerText = "";
+            display(images, heroEl);
+        }).catch(err => {
+            console.log('invalid search')
+        })
 
 })
 
