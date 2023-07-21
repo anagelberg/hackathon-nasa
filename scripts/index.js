cbd9915974ff060c8310afb855d6e25cb97a444d
anagelberg <amy.nagelberg@gmail.com>
2023-07-21 10:54:48 -0700

    merge html with js form entry

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,11 @@
-
-
-
 const display = (imageUrl, container) => {
     container.style.backgroundImage = imageUrl;
 }
 
 
-const form = document.querySelector('.form');
-
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log(event.target.input.value);
-    //search the axios
+    console.log(event.target.name.value);
+
 })
 
